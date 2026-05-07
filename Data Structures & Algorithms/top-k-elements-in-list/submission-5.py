5666fdd79b2ea925561c629024fce1798d09af86
Jesse Jiang <106723864+JuicinessJ@users.noreply.github.com>
2026-05-07 01:39:04 -0500

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        map = {}
+
+        for num in nums:
+            if num not in map:
+                map[num] = 0
+            map[num] += 1
+        return [key for key, value in sorted(map.items(), reverse=True, key=lambda x: x[1])][:k]
+        # .items()... gets (key, value)
+        # reverse=True... sort by descending order, most frequent k
+        # key=lambda x: x[1]... passes in value to be sorted instead of key(x[0])
+        # [:k]... splice list from 0 to k
+        # key for key, value in... unpacks tuple (key, value) and only return key variable, discarding v
