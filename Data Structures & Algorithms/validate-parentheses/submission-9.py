e2cb5c7a36d73aca840b30f1d3b0497da186f173
Jesse Jiang <106723864+JuicinessJ@users.noreply.github.com>
2026-05-13 17:31:55 -0500

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        map = {")": "(", "}": "{", "]": "["}
+        stk = []
+
+        for c in s:
+
+            if c in map and len(stk) == 0:
+                return False
+
+            if c in map:
+
+                pop = stk.pop()
+
+                if map[c] != pop:
+                    return False
+                
+            else:
+                stk.append(c)
+
+        if len(stk) != 0:
+            return False
+            
+        return True
