3372b37f439ee6ccbadcdfc8d142eb49d8b15421
Jesse Jiang <106723864+JuicinessJ@users.noreply.github.com>
2026-05-13 20:41:36 -0500

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stk = []
+
+        highest_temp = 0
+
+        for i, temp in enumerate(temperatures):
+
+            while stk and stk[-1][0] < temp:
+
+                stk_temp, stk_index = stk.pop()
+
+                res[stk_index] = i - stk_index
+
+            stk.append([temp, i])
+
+        return res
