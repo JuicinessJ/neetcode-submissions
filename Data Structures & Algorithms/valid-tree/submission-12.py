4c22d9f27d2bf2e7a61a700b6d2243ffe422d495
Jesse Jiang <106723864+JuicinessJ@users.noreply.github.com>
2026-07-31 18:11:11 -0500

    Add: valid-tree - submission-12

---
+++ Data Structures & Algorithms/valid-tree/submission-12.py
@@ -0,0 +1,35 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if not n:
+            return True
+
+        adj_lst = { i: [] for i in range(n) }
+
+        for key, value in edges:
+            if key not in adj_lst:
+                adj_lst[key] = []
+
+            if value not in adj_lst:
+                adj_lst[value] = []
+
+            adj_lst[key].append(value)
+            adj_lst[value].append(key)
+
+        visited = set()
+
+        def dfs(node, parent):
+            if node in visited:
+                return False
+
+            visited.add(node)
+
+            for edge in adj_lst[node]:
+                if edge == parent:
+                    continue
+
+                if not dfs(edge, node):
+                    return False
+
+            return True
+
+        return dfs(0, -1) and len(visited) == n
